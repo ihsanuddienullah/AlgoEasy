ec6418187b9cfb762a531c54f777d38493e917df
ihsanuddienullah <ihsanuddienullah@gmail.com>
2026-05-01 15:49:45 +0700

    feat: migrate Content to TypeScript functional component

---
+++ src/components/Content.tsx
@@ -0,0 +1,15 @@
+import About from './content/About'
+import Lesson from './content/Lesson'
+import Action from './content/Action'
+import Contact from './content/Contact'
+
+export default function Content() {
+  return (
+    <div className="content">
+      <About />
+      <Lesson />
+      <Action />
+      <Contact />
+    </div>
+  )
+}
