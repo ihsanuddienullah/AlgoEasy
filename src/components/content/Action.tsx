86a6604b7826f4eb98ca80871026d2b3af32b530
ihsanuddienullah <ihsanuddienullah@gmail.com>
2026-05-01 16:48:05 +0700

    feat: migrate Action to TypeScript functional component

---
+++ src/components/content/Action.tsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
+export default function Action() {
+  return (
+    <div id="action">
+      <div className="action-desc">
+        <Container>
+          <Row>
+            <Col>
+              <h3>Ready to become an awesome programmer?</h3>
+              <p>Let's learn to code, learn to be creative!</p>
+              <Button variant="primary" href="https://course.refactory.id">Start Learning</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  )
+}
