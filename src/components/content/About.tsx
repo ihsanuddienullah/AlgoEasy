5d9f7db0f99cafb0288b86021c64ef4b0c26d582
ihsanuddienullah <ihsanuddienullah@gmail.com>
2026-05-01 16:47:30 +0700

    feat: migrate About to TypeScript functional component

---
+++ src/components/content/About.tsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function About() {
+  return (
+    <div id="about-us">
+      <div className="about-desc">
+        <Container>
+          <Row>
+            <Col>
+              <h1>About Us</h1>
+              <p>
+                About AlgoEasy When we started AlgoEasy, our goal was to give anyone in the world
+                the ability to learn the skills they'd need to succeed in the 21st century. We set
+                out to create a new, interactive way of learning — making it engaging, flexible, and
+                accessible for as many people as possible. Since then, we have helped millions of
+                people worldwide unlock modern technical skills and reach their full potential through
+                code.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  )
+}
